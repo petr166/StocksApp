44f93f98f866df77110d3dae0af345002b32fd71
Petru Birzu <petru.birzu96@gmail.com>
2016-10-31 13:06:07 +0100

    add 'buy offer' functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -195,6 +195,7 @@ function handleSellButt() {
 
         var offers = jsonObj.data;
 
+        $('#offersTable tbody').html('');
         offers.forEach(function(current) {
           var row = $("<tr>");
           var keysByIndex = Object.keys(current);
@@ -203,6 +204,13 @@ function handleSellButt() {
             var cellData = current[keysByIndex[i]];
             row.append($("<td>" + cellData + "</td>"));
           }
+
+          var offerID = current.id;
+
+          var buyButt = $('<button class="btn btn-success" type="button" onclick="handleBuyButt(' + offerID + ')">Buy</button>');
+          row.append(buyButt);
+          var successBuyAlert = $('<span id="successBuyAlert" class="alert alert-success fade in" role="alert"><strong>Success!</strong></span>');
+          row.append(successBuyAlert);
 
           $('#offersTable tbody').append(row);
         })
@@ -259,6 +267,55 @@ function handleSellButt() {
 
         $("#exchangeRate h3").val('');
         $("#exchangeRate h3").html("1 " + globCurrency + " = " + rate + " " + toCurrency);
+     }
+   });
+ }
+
+ // buy offer req: ~?what=buy&offer={id}&apikey={key}
+ function handleBuyButt(offerID) {
+  $.ajax({
+     'url' : apiURL,
+     'type' : 'GET',
+     'data' : {
+       'what' : 'buy',
+       'offer' : offerID,
+       'apikey' : apiKEY
+     },
+
+     // handle server response
+     'success' : function(dataIN) {
+       //create the JSON object
+       var jsonObj = JSON.parse(dataIN);
+
+       /* RESULT
+       {
+          "req":{
+             "code":"200",
+             "status":"OK",
+             "method":"GET"
+          },
+          "user":{
+             "name":"SKYX0002",
+             "apikey":"***************"
+          },
+          "what":"buy",
+          "data":{
+             "id":"2",
+             "offerCurrency":"NICO381M",
+             "buyCurrency":"SKYX0002",
+             "amount":"1.36",
+             "offerTime":"2016-10-23 21:58:41",
+             "buyTime":"2016-10-23 22:02:12"
+          }
+       }
+       */
+
+        console.log("Buy object received:");
+        console.log(jsonObj);
+
+        $('.alert').hide();
+        $('#successBuyAlert').show();
+        setTimeout(reqGetOffers(), 4000);
      }
    });
  }
